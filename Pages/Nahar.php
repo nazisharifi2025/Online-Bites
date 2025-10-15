ce2565b2b8a81342ea244382abf7b930bd878c26
nazi sharifi <banonazisharifi@gmail.com>
2025-10-16 00:41:35 +0430

    first commit added

---
+++ Pages/Nahar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../src/output.css">
+</head>
+<body>
+    <div class="flex justify-between px-4 items-center w-full h-20 text-white bg-red-300">
+        <h1 class="text-2xl font-bold">رستورانت آنلاین</h1>
+        <ul class="flex space-x-6 justify-center items-center font-bold text-white">
+            <li><a href="home.php">صفحه اصلی</a></li>
+            <li><a href="">دسته بندی ها </a></li>
+            <li><a href="">غذا ها</a></li>
+            <li><a href="">درباره ما</a></li>
+            <li><a href="">تماس با ما </a></li>
+            <li><a href="">بخش مدریت</a></li>
+            <li><a href="">عضویت</a></li>
+            <li><a href="">ورود</a></li>
+        </ul>
+    </div>
+</body>
+</html>
---
+++ Pages/home.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../src/output.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body dir="rtl">
+    <?php include "Nahar.php" ?>
+ <div class="h-[60vh] w-full bg-no-repeat bg-cover bg-center bg-[url(../images/Hero2.webp)]">
+    <div class="h-full w-full flex flex-col gap-6 justify-center items-center bg-black/25">
+        <h1 class="text-3xl font-bold">مواد غذایی  با کیفیت در درب شما</h1>
+        <p class="font-bold text-xl">ما کیفت را تحویل میدهیم و سالهاست که این کار را  انجام میدهیم!</p>
+    </div>
+ </div>
+ <!-- Navbar Endedd -->
+  <div class=" flex flex-col justify-center items-center gap-12 my-12">
+    <h1 class=" text-4xl font-bold">کادر مجرب رستوران آماده سرویس دهی</h1>
+    <div class="flex justify-center  gap-12 w-[60%] mx-auto h-fit">
+        <img src="../images/images1.jpeg" class="h-64 w-[40%]" alt="">
+        <p class="text-xl">
+            خوراک یا غذا خوراکی‌ است که در جوامع مختلف، به صورت جامد یا مایع تهیه شده و برای مصرف آماده می‌شود.  
+خوراک ممکن است خام یا پخته باشد، با دست پخته یا بسته‌بندی شده، یا از بیرون تهیه شده باشد.  
+غذا می‌تواند شامل خوراکی‌های سنتی، مدرن، خانگی یا صنعتی باشد.  
+
+غذا نقش مهمی در فرهنگ، اقتصاد، سلامت و اجتماع ایفا می‌کند و از اجزای مهم زندگی روزمره است.
+        </p>
+
+    </div>
+  </div>
+  <!-- div 2 endded -->
+   <div class=" w-[90%] h-[60vh] my-12 mx-auto grid grid-cols-3 gap-6">
+    <div class="h-full shadow-md shadow-gray-500 flex-col items-start justify-center gap-5">
+        <img src="../images/darya.jpg" class="w-full h-[70%] " alt="">
+        <h1 class="text-3xl px-4">غذای دریایی</h1>
+        <p class="px-4">آیا میخواهید غذاهای این دسته را بررسی نمایید؟ اکنون کاوش کنید!</p>
+    </div>
+    <!-- one d endded -->
+      <div class="h-full shadow-md shadow-gray-500 flex-col items-start justify-center gap-5">
+        <img src="../images/ast.jpg" class="w-full h-[70%] " alt="">
+        <h1 class="text-3xl px-4">فست فود</h1>
+        <p class="px-4">آیا میخواهید غذاهای این دسته را بررسی نمایید؟ اکنون کاوش کنید!</p>
+    </div>
+    <!-- tow d endded -->
+       <div class="h-full shadow-md shadow-gray-500 flex-col items-start justify-center gap-5">
+        <img src="../images/af.jpg" class="w-full h-[70%] " alt="">
+        <h1 class="text-3xl px-4">غذاهای افغانی</h1>
+        <p class="px-4">آیا میخواهید غذاهای این دسته را بررسی نمایید؟ اکنون کاوش کنید!</p>
+    </div>
+   </div>
+   <!-- Foods Endded -->
+    <div class=" flex flex-col justify-center h-fit w-full items-center gap-8">
+        <h1 class="text-3xl font-bold">درباره ما </h1>
+        <p class="text-xl text-center w-[60%]">هدف اصلی فروشگاه آنلاین ما، فراهم آوردن تجربه‌ای متمایز از سفارش غذا برای مشتریان عزیز است. ما با بهره‌گیری از کادر مجرب آشپزی و استفاده از مواد اولیه تازه و با کیفیت، تلاش می‌کنیم تا غذاهایی لذیذ، سالم و متنوع را در کوتاه‌ترین زمان ممکن به دست شما برسانیم.
+
+ما متعهد به ارائه بهترین خدمات، بسته‌بندی بهداشتی، تحویل سریع و پشتیبانی پاسخگو هستیم تا تجربه‌ای دلنشین از سفارش آنلاین غذا برای شما رقم بزنیم. رضایت شما، افتخار ماست</p>
+<div class="w-[60%] flex justify-between items-center">
+    <div class="flex flex-col items-center justify-center gap-3">
+        <i class="fas fa-smile text-5xl"></i>
+        <p class="text-xl">کیفیت عالی</p>
+    </div>
+    <div class="flex flex-col items-center justify-center gap-3">
+        
+     <i class="fas fa-truck text-5xl"></i>
+        <p class="text-xl">کیفیت عالی</p>
+    </div>
+    <div class="flex flex-col items-center justify-center gap-3">
+        <i class="fas fa-utensils text-5xl"></i>
+        <p class="text-xl">کیفیت عالی</p>
+    </div>
+</div>
+    </div>
+    <!-- about Us Endded -->
+     <div class="h-[70vh] w-full flex justify-center  items-center space-x-12">
+             <div class="h-[40vh] shadow-md shadow-gray-400 px-4 w-[190px] gap-6 relative  text-white rounded-md text-center flex bg-red-400/80 flex-col justify-center items-center">
+            <p>غذاهایی این رستورانت بهترین غذایی است که تا بحل ازش لذت بردم حتما امتحان منید!</p>
+            <strong class="font-bold">علی محمدی</strong>
+             <div class="h-[35vh] shadow-md shadow-gray-400 absolute -right-[120px] -z-20 w-[140px] gap-6 text-white rounded-md text-center flex bg-red-400/40 flex-col justify-center items-center ">
+            <p>غذایی این رستورانت  دقیقا مثل بهشت بوده است آنقدر تازه و خوشمزه است که نمیتوانم هر هفته به آنجا سفر کنم!</p>
+            <strong class="font-bold">محمد عالمی</strong>
+        </div>
+        </div>
+        <!-- div 1 -->
+        <div class="h-[50vh] w-[230px] shadow-md shadow-gray-400 gap-6 text-white rounded-md text-center flex bg-red-400 flex-col justify-center items-center text-xl">
+            <p>غذایی این رستورانت  دقیقا مثل بهشت بوده است آنقدر تازه و خوشمزه است که نمیتوانم هر هفته به آنجا سفر کنم!</p>
+            <strong class="font-bold">احمد رشیدی</strong>
+        </div>
+        <!-- dev 2 -->
+             <div class="h-[40vh] shadow-md shadow-gray-400 w-[190px] gap-6 relative  text-white rounded-md text-center flex bg-red-400/80 flex-col justify-center items-center">
+            <p>غذایی این رستورانت  دقیقا مثل بهشت بوده است آنقدر تازه و خوشمزه است که نمیتوانم هر هفته به آنجا سفر کنم!</p>
+            <strong class="font-bold">صادق باقری</strong>
+             <div class="h-[35vh] shadow-md shadow-gray-400 absolute -left-[120px] -z-20 w-[140px] gap-6 text-white rounded-md text-center flex bg-red-400/40 flex-col justify-center items-center ">
+            <p>غذایی این رستورانت  دقیقا مثل بهشت بوده است آنقدر تازه و خوشمزه است که نمیتوانم هر هفته به آنجا سفر کنم!</p>
+            <strong class="font-bold">مهدی نایاب</strong>
+        </div>
+        </div>
+        <!-- div 3 -->
+     </div>
+     <!-- Testemonial endded -->
+      <div class=" w-full h-[40vh] bg-red-200 flex flex-col justify-center items-center ">
+        <div class="w-full h-[80%] border-b border-white flex justify-between items-center px-12">
+            <div class="flex flex-col gap-4">
+            <h1 class="font-bold text-2xl text-white">شبکه های اجتاعی</h1>
+            <div class="flex space-x-5">
+                <button class="p-3 rounded-full bg-white text-red-300">
+                    <i class="fab fa-facebook-f"></i>
+                </button>
+                <button class="p-3 rounded-full bg-white text-red-300">
+                    <i class="fab fa-instagram"></i>
+                </button>
+                <button class="p-3 rounded-full bg-white text-red-300">
+                    <i class="fab fa-twitter"></i>
+                </button>
+                <button class="p-3 rounded-full bg-white text-red-300">
+                    <i class="fab fa-telegram-plane"></i>
+                </button>
+                <button class="p-3 rounded-full bg-white text-red-300">
+                    <i class="fab fa-whatsapp"></i>
+                </button>
+                <button class="p-3 rounded-full bg-white text-red-300">
+                    <i class="fab fa-linkedin-in"></i>
+                </button>
+                </div>
+                <!-- div 1 endded -->
+                 
+            </div>
+            <div class="flex flex-col text-white gap-4">
+                    <h1 class="text-2xl " >درباره ما</h1>
+                  <span class="text-xl">   مکان: جاغوری بازار حوتقول</span>
+                  <span class="text-xl">تلفن: +93 795064649</span>
+                  <span class="text-xl">سرویس:  همه روزه در طول ایام هفته</span>
+                 </div>
+        </div>
+        <h1 class="text-xl text-center text-white my-2">با ما همراه باشید!</h1>
+      </div>
+</body>
+</html>
